7afac4db41e2c52f37e6125bcc9cb0468a54d21e
Joonas Pelttari <joonas.pelttari98@gmail.com>
2019-10-07 02:57:51 +0300

    Oikea kaav

--- GIT_EX/temperature.py
+++ GIT_EX/temperature.py
@@ -9,22 +9,12 @@ def main():
 
 
     print("{} C = {} F".format(input_temp, t_transform(input_temp, 'c')))
-    print("{} F = {} C".format(input_temp, FtoC_transform(input_temp, 'f')))
-
-
 
 
 def t_transform(temp, type):
 
     if type == 'c':
-        return float(temp * 9/15 + 32)
-
-    return None
-
-def FtoC_transform(temp, type):
-
-    if type == 'f':
-        return float((temp - 32) * 15/9)
+        return float(temp * 9/5 + 32)
 
     return None
 
