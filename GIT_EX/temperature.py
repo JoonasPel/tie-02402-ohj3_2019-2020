b4b9ba317d4adaf12afe0b61d2d120de2e000dd8
Joonas Pelttari <joonas.pelttari98@gmail.com>
2019-10-07 02:04:09 +0300

    Muutettu lämpötilajuttua.

--- GIT_EX/temperature.py
+++ GIT_EX/temperature.py
@@ -9,6 +9,7 @@ def main():
 
 
     print("{} C = {} F".format(input_temp, t_transform(input_temp, 'c')))
+    print("{} F = {} C".format(input_temp, FtoC_transform(input_temp, 'f')))
 
 
 
@@ -17,6 +18,13 @@ def t_transform(temp, type):
 
     if type == 'c':
         return float(temp * 9/15 + 32)
+
+    return None
+
+def FtoC_transform(temp, type):
+
+    if type == 'f':
+        return float((temp - 32) * 15/9)
 
     return None
 
