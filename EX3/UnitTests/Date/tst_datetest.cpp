c208c2eaa094277f04eb2e37d240226416c679ce
Joonas Pelttari <joonas.pelttari98@gmail.com>
2019-10-13 20:15:19 +0300

    Joko nyt :D Kaikki tehty uudestaan

---
+++ EX3/UnitTests/Date/tst_datetest.cpp
@@ -0,0 +1,26 @@
+#include <QString>
+#include <QtTest>
+
+class DateTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    DateTest();
+
+private Q_SLOTS:
+    void testCase1();
+};
+
+DateTest::DateTest()
+{
+}
+
+void DateTest::testCase1()
+{
+    QVERIFY2(true, "Failure");
+}
+
+QTEST_APPLESS_MAIN(DateTest)
+
+#include "tst_datetest.moc"
--- EX3/UnitTests/UnitTests.pro
+++ EX3/UnitTests/UnitTests.pro
@@ -1,4 +1,5 @@
 TEMPLATE = subdirs
 
 SUBDIRS += \
-    Morottaja
+    Morottaja \
+    Date
